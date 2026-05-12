888ba01af4894c41666e6c09358f964e85d120b6
kiduyu-klaus <muigakiduyu1995@gmail.com>
2026-05-12 05:19:51 +0300

    Fix some import issues

--- app/src/main/java/com/kiduyuk/klausk/kiduyutv/ui/screens/detail/mobile/MobileMovieDetailScreen.kt
+++ app/src/main/java/com/kiduyuk/klausk/kiduyutv/ui/screens/detail/mobile/MobileMovieDetailScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.PlayArrow
